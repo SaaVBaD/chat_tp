a0a2900be1a2def41afc7ae5864eef8f8f11006e
SaaVBaD <savbad@offconcern.com>
2023-05-19 20:02:02 +1000

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function openForm() {
+    document.getElementById("chat").style.display = "block";
+    document.getElementById("open").style.display = "none";
+    document.getElementById("close").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("chat").style.display = "none";
+}
